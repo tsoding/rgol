6611e8892d95ac74bb5372330559e396f7ea6944
~~~ <flutterlice@gmail.com>
2020-07-24 22:37:45 +0500

    Feels free to ignore

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <GLFW/glfw3.h>
+
+#define USE_GLFW 1
 
 typedef enum {
     DED = 0,
@@ -9,6 +12,10 @@ typedef enum {
 #define WIDTH 5
 #define HEIGHT 5
 
+GLFWwindow * window;
+const int W_WIDTH = 640;
+const int W_HEIGHT = 480;
+
 typedef struct {
     Cell cells[HEIGHT][WIDTH];
 } Board;
@@ -16,6 +23,18 @@ typedef struct {
 int modi(int x, int y)
 {
     return (x % y + y) % y;
+}
+
+int to_update_state = 0;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
+      to_update_state = 1;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+      glfwTerminate();
+      exit(0);
+    }
 }
 
 void board_display(const Board *board, FILE *stream)
@@ -27,6 +46,33 @@ void board_display(const Board *board, FILE *stream)
         }
         fputc('\n', stream);
     }
+}
+
+void board_display_glfw(const Board *board)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_QUADS);
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            if(board->cells[y][x] == ALIVE) {
+              float size_x = (1.9f)/WIDTH; 
+              float size_y = (1.9f)/WIDTH; 
+              float top_left_x = (float)(x*2)/WIDTH - 1.0f;
+              float top_left_y = -((float)(y*2)/WIDTH - 1.0f);
+              glVertex3f(top_left_x, top_left_y, 0.0f);
+              glColor3f(1.0f, 0.0f, 0.0f);
+              glVertex3f(top_left_x+size_x, top_left_y, 0.0f);
+              glColor3f(1.0f, 0.0f, 0.0f);
+              glVertex3f(top_left_x+size_x, top_left_y-size_y, 0.0f);
+              glColor3f(1.0f, 0.0f, 0.0f);
+              glVertex3f(top_left_x, top_left_y-size_y, 0.0f);
+              glColor3f(1.0f, 0.0f, 0.0f);
+            }
+        }
+    }
+    glEnd();
+    glfwSwapBuffers(window);
 }
 
 int board_neighbors(const Board *board, int y, int x)
@@ -61,9 +107,9 @@ void board_next_gen(const Board *prev, Board *next)
 Board board[2] = {
     {
         .cells = {
-            {1, 1, 1, 0, 0},
-            {1, 0, 0, 0, 0},
-            {0, 1, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 0, 1, 0, 0},
+            {0, 1, 1, 0, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0},
         }
@@ -71,21 +117,61 @@ Board board[2] = {
     {}
 };
 
+void terminate_glfw() {
+    glfwTerminate();
+}
+
+int fb = 0;
+
+void window_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+    board_display_glfw(&board[fb]);
+}
+
+void init_glfw() {
+  glfwInit();
+  window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "GOL", NULL, NULL);
+  glfwSetWindowSizeCallback(window, window_size_callback);
+  glfwSetKeyCallback(window, key_callback);
+  glfwMakeContextCurrent(window);
+}
+
 int main(int argc, char *argv[])
 {
-    int fb = 0;
+    init_glfw();
+    #ifdef USE_GLFW
+    board_display_glfw(&board[fb]);
+    #else
     board_display(&board[fb], stdout);
     fputs("------------------------------\n", stdout);
+    #endif
 
 #define GLIDER_CYCLES 4
 #define STEPS (GLIDER_CYCLES * 5)
     for (int i = 0; i < STEPS; ++i) {
+        #ifdef USE_GLFW
+        int bb = 1 - fb;
+        board_display_glfw(&board[fb]);
+        while (1) {
+          glfwPollEvents();
+          usleep(10000);
+          if(to_update_state == 1) {
+            to_update_state = 0;
+            break;
+          }
+        }
+        board_next_gen(&board[fb], &board[bb]);
+        fb = bb;
+        #else
         int bb = 1 - fb;
         board_next_gen(&board[fb], &board[bb]);
         fb = bb;
         board_display(&board[fb], stdout);
         fputs("------------------------------\n", stdout);
+        #endif
     }
 
+    terminate_glfw();
     return 0;
 }
